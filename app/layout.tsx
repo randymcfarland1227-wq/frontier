fc36f69d6dfc0e555fa6b7bc8389dc68047367cc
randymcfarland1227-wq <randymcfarland1227@gmail.com>
2026-09-17 01:04:24 -0400

    Add Home Screen web app support

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="./manifest.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+        <meta name="theme-color" content="#5968e8" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Randy’s Work Room" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
